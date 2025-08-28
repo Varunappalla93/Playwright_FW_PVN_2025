3ab4b65b44a11bbc13cfe1d98577163ad25c9e5f
varun appalla <an.varun1993@gmail.com>
2025-08-28 10:08:47 +0530

    Githubactions

--- tests/EndToEndTest.spec.ts
+++ tests/EndToEndTest.spec.ts
@@ -178,4 +178,4 @@ async function performCheckout(page: Page) {
 
 // if git push to remote repo fails, use the below and then use push command 
 // git remote set-url origin https://<token_name>@github.com/<username>/<reponame> 
-// Eg: git remote set-url origin https://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@github.com/Varunappalla93/Playwright_FW_PVN_2025
+// Eg: git remote set-url origin https://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@github.com/Varunappalla93/Playwright_FW_PVN_2025
